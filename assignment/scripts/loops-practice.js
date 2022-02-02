3dae6eedad17c84faff67e00d579e843680a2aa3
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-02 14:53:03 -0600

    added changes to part 2 of week 3 assignment

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -56,17 +56,18 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-while (stars.length<=5) {
-  console.log(stars);
-  stars.length++;
+let i=0
+while (i<5) {
+  console.log(stars[i]);
+  i++;
 }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-let i=0
-while (i<6) {
-  console.log(i);
-  i++;
+let I=0
+while (I<6) {
+  console.log(I);
+  I++;
 }
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
